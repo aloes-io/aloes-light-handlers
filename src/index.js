6001d44d626b3f0ebce3fb4a1ee83cf009ad74d5
getlarge <ed@getlarge.eu>
2020-07-17 08:10:35 +0200

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const {aloesLightEncoder} = require('./lib/encoder');
 const {aloesLightDecoder} = require('./lib/decoder');
 const {aloesLightPatternDetector} = require('./lib/detector');
+const version = require('../package.json').version;
 
 module.exports = {
+  version,
   aloesLightEncoder,
   aloesLightDecoder,
   aloesLightPatternDetector,
